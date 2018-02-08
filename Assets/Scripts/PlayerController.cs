e2b9d80aab4a176705c0d055ec319f35c80a0ab8
Anthony Navarro <anthony.navarro@student.csulb.edu>
2018-02-08 12:15:08 -0800

    JumpV2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,30 @@ public class PlayerController : MonoBehaviour {
 
 
 	public int jumpHeight;
+	public bool canJump;
+
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Jump")){
-			Jump ();
+		if (Input.GetButtonDown ("Jump")) {
+			if (canJump == true) {
+				Jump ();
+			}
 		}
 	}
+
+
+	void OnCollisionEnter2D (Collision2D other) 
+	{
+		canJump = true;
+	}
+
+	void OnCollisionExit2D (Collision2D other) 
+	{
+		canJump = false;
+	}
+
+
 
 	void Jump(){
 		GetComponent<Rigidbody2D> ().AddForce (Vector2.up * jumpHeight);
