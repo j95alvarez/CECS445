96e6d3ef52a079f94b6c5c5145d1f8bc00b3a2d0
j95alvarez <juanalvarez391@yahoo.com>
2018-04-26 21:11:06 -0700

    Adding google play code

---
+++ Assets/Scripts/GooglePlay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooglePlay : MonoBehaviour {
+    public static void AddScoreToGlobalLeaderboard(int score) {
+        #if UNITY_ANDROID && !UNITY_EDITOR
+            if (PlayerPrefs.GetInt("OptOut", 0) == 0)
+            {
+                if (connectedToGooglePlay)
+                {
+                    Social.ReportScore(score, GPGSIds.leaderboard_global_leaderboard, success => { });
+                }
+
+                else
+                {
+                    Social.localUser.Authenticate((bool success) => { connectedToGooglePlay = success; });
+                }
+            }
+        #endif
+    }
+
+    public static void InitializeGooglePlay() {
+        #if UNITY_ANDROID && !UNITY_EDITOR
+            //sign in to Google Play Services
+            PlayGamesPlatform.Activate();
+
+            if (!LeaderBoardController.connectedToGooglePlay)
+            {
+                Social.localUser.Authenticate((bool success) =>
+                {
+                    LeaderBoardController.connectedToGooglePlay = success;
+                });
+            }
+        #endif
+    }
+}
