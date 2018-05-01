6282438ab7b31623e490bda24df266fc3eabaf27
j95alvarez <juanalvarez391@yahoo.com>
2018-05-01 11:35:33 -0700

    adding google play stuff

--- Assets/Scripts/GooglePlay.cs
+++ Assets/Scripts/GooglePlay.cs
@@ -1,37 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
 
 public class GooglePlay : MonoBehaviour {
-    public static void AddScoreToGlobalLeaderboard(int score) {
-        #if UNITY_ANDROID && !UNITY_EDITOR
-            if (PlayerPrefs.GetInt("OptOut", 0) == 0)
-            {
-                if (connectedToGooglePlay)
-                {
-                    Social.ReportScore(score, GPGSIds.leaderboard_global_leaderboard, success => { });
-                }
 
-                else
-                {
-                    Social.localUser.Authenticate((bool success) => { connectedToGooglePlay = success; });
-                }
-            }
+    private void Start()
+    {
+        InitializeGooglePlay();
+    }
+
+    public static void AddScoreToGlobalLeaderboard(int score) {
+        #if UNITY_ANDROID
+                Social.ReportScore(score, GPGSIds.leaderboard_top_runners, success => { });
         #endif
     }
 
     public static void InitializeGooglePlay() {
-        #if UNITY_ANDROID && !UNITY_EDITOR
+        #if UNITY_ANDROID
             //sign in to Google Play Services
             PlayGamesPlatform.Activate();
-
-            if (!LeaderBoardController.connectedToGooglePlay)
-            {
-                Social.localUser.Authenticate((bool success) =>
-                {
-                    LeaderBoardController.connectedToGooglePlay = success;
-                });
-            }
+        
+            Social.localUser.Authenticate((bool success) => { });
         #endif
     }
 }
